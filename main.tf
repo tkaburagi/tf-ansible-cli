cfa81974a574271c92610753edf0022cbe98d513
tkaburagi <t.kaburagi@me.com>
2021-06-14 17:44:18 +0900

    first commit

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
 resource "null_resource" "execute-aws-cli" {
-
-	triggers {
-		db_host = 
-	}
-	provisioner "local-exec" {
-		inline = [
-			"sudo apt update",
-			"ansible --version"
-		]
-	}
+  provisioner "local-exec" {
+    inline = [
+      "sudo apt update",
+      "ansible --version"
+    ]
+  }
 }
