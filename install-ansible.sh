4ca6b7803b079a0f87a2db432b99c3bce8e5e032
tkaburagi <t.kaburagi@me.com>
2021-06-14 17:53:47 +0900

    first commit

--- install-ansible.sh
+++ install-ansible.sh
@@ -2,5 +2,5 @@
 sudo apt update --yes
 sudo apt install --yes software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible --yes
 ansible --version
